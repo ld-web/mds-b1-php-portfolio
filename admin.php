4fa64aa889d9c03ed8c9dc3aecefeb8e983df935
Lucas Delobelle <lucas@ld-web.net>
2021-03-25 09:14:36 +0100

    Skeleton App

---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'layout/header.php';
+?>
+
+<div class="container">
+  <h1>Administration des réalisations</h1>
+</div>
+
+<?php
+require_once 'layout/footer.php';
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'layout/header.php';
+?>
+
+<div class="container">
+  <h1>Bienvenue sur le portfolio</h1>
+</div>
+
+<?php
+require_once 'layout/footer.php';
